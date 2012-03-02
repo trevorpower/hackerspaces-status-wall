a5b1f28cbfb6846c225107a63f6b05ddf38b9dfb
Trevor Power <trevor_power@yahoo.com>
2012-03-02 23:24:41 +0000

    handle ssl connections to other api

--- fetch-status.rb
+++ fetch-status.rb
@@ -2,22 +2,32 @@ require 'json'
 
 def fetchStatus uri, limit = 5
   return {:error => "To many HTTP redirects"}.to_json if limit == 0
-  response = Net::HTTP.get_response(uri)
-  case response
-  when Net::HTTPSuccess then
-    response.body
-  when Net::HTTPRedirection then
-    location = URI.parse(response['Location'])
-    location = uri.merge(location) if location.relative?
-    fetchStatus location, limit - 1
-  else
-    {:error => "#{response.value}"}.to_json
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = uri.scheme == 'https'
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  http.start do |http|
+    handleResponse uri, http.request_get(uri.request_uri), limit
   end
 rescue => e
-  if uri.scheme == 'https'
-    uri.scheme = 'http'
-    fetchStatus uri, limit
-  else
-    {:error => e.message}.to_json
+  #return tryHttp uri, limit if uri.scheme == 'https'
+  {:error => e.message}.to_json
+end
+
+def handleResponse uri, response, limit
+  case response
+  when Net::HTTPSuccess then response.body
+  when Net::HTTPRedirection then followRedirection uri, response, limit
+  else {:error => response.value}.to_json
   end
+end
+
+def followRedirection uri, response, limit
+  location = URI.parse(response['Location'])
+  location = uri.merge(location) if location.relative?
+  fetchStatus location, limit - 1
+end
+
+def tryHttp uri, limit
+  uri.scheme = 'http'
+  fetchStatus uri, limit
 end
