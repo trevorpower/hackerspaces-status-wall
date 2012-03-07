4f1673a79581953ac2c7f18f805ad065f1ed9286
Trevor Power <trevor_power@yahoo.com>
2012-03-07 13:56:21 +0000

    use Faraday to implement proxy

--- proxy.rb
+++ proxy.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'net/http'
 require 'dalli'
+require 'faraday_middleware'
 
 class Proxy < Sinatra::Base
   
@@ -9,40 +10,13 @@ class Proxy < Sinatra::Base
   post '/proxy' do
     headers 'Content-Type' => 'application/json'
     url = request.body.read
-    settings.cache.fetch(url, 300) do
-      puts "proxy request not cached: #{url}"
-      fetchStatus URI.parse url
+    settings.cache.fetch url, 180 do
+      puts "#{url}: not cached"
+      connection = Faraday.new url, :ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'} do |conn|
+        conn.use FaradayMiddleware::FollowRedirects
+        conn.adapter :net_http
+      end
+      connection.get.body
     end
   end
-
-  def fetchStatus uri, limit = 5
-    return {:error => "To many HTTP redirects"}.to_json if limit == 0
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = uri.scheme == 'https'
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    http.read_timeout = 25
-    http.start do |http|
-      handleResponse uri, http.request_get(uri.request_uri), limit
-    end
-  rescue => e
-    {:error => e.message}.to_json
-  end
-
-  def handleResponse uri, response, limit
-    case response
-    when Net::HTTPSuccess
-      response.body
-    when Net::HTTPRedirection
-      followRedirection uri, response, limit
-    else
-      {:error => response.value}.to_json
-    end
-  end
-
-  def followRedirection uri, response, limit
-    location = URI.parse(response['Location'])
-    location = uri.merge(location) if location.relative?
-    fetchStatus location, limit - 1
-  end
-
 end
